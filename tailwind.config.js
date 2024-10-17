cc562bedf3258af4a55c25d329253859d6024dbc
Alex-new-laptop <acontell@gmail.com>
2024-10-17 15:48:05 +0200

    Added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        darkGrey: '#282828',
+        yellow: '#F69425',
+        blue: '#67AADE',
+        red: '#CD320C',
+        offWhite: '#F3E3C1',
+        green: '#2B7723',
       },
     },
   },
