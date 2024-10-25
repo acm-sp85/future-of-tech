4d6280d30937b6377ce627cb5046440bf959174a
Alex-new-laptop <acontell@gmail.com>
2024-10-25 16:37:36 +0200

    created custom styles for H tags

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,68 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="bg-darkGrey h-screen">
-      The Future of Tech
-      <div className=" w-16 h-16 bg-green font-inter font-bold">GREEN</div>
-      <div className=" w-16 h-16 bg-yellow font-jura">YELLOW</div>
-      <div className=" w-16 h-16 bg-red">Red</div>
-      <div className=" w-16 h-16 bg-blue">Blue</div>
-      <div className=" w-16 h-16 bg-offWhite font-italiana">Off White</div>
+    <div className="bg-darkGrey flex h-5000">
+      {/* <div className="w-[10%] bg-green h-8 flex items-center justify-center">
+        1
+      </div>
+      <div className="w-[80%] bg-yellow h-8 flex items-center justify-left">
+        <div className="flex items-center justify-between">
+          <div
+            className=" w-32
+        
+        bg-green font-inter font-bold flex items-center justify-center"
+          >
+            GREEN
+          </div>
+          <div
+            className=" w-32
+        
+        bg-yellow font-jura flex items-center justify-center"
+          >
+            YELLOW
+          </div>
+          <div
+            className=" w-32
+        
+        bg-red flex items-center justify-center"
+          >
+            Red
+          </div>
+          <div
+            className=" w-32
+        
+        bg-blue flex items-center justify-center"
+          >
+            Blue
+          </div>
+          <div
+            className=" w-32
+        
+        bg-offWhite font-italiana flex items-center justify-center"
+          >
+            Off White
+          </div>
+        </div>
+      </div>
+      <div className="w-[10%] bg-blue h-8 flex items-center justify-center">
+        3
+      </div> */}
+      <div>
+        <h1>This is an H1 tag</h1>
+        <h2>This is an H2 tag</h2>
+        <h3>This is an H3 tag</h3>
+        <h4>This is an H4 tag</h4>
+        <h5>This is an H5 tag</h5>
+        <h6>This is an H6 tag</h6>
+        <p>This is a P tag</p>
+        <h8>This is an H8 tag</h8>
+        <br></br>
+        <h9>This is an H9 tag</h9>
+        <br></br>
+        <h10>This is an H10 tag</h10>
+        <br></br>
+        <h11>This is an H11 tag</h11>
+      </div>
     </div>
   );
 }
