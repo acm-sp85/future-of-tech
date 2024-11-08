032c146ea7c408bb8f8d854ca8d944436d3e774c
Alex-new-laptop <acontell@gmail.com>
2024-11-08 14:31:56 +0100

    new sections added

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="bg-darkGrey  h-5000">
+      {/* NAV BAR */}
       <nav className="flex sticky top-5 w-full">
         <div className="w-[15%] h-8 flex items-center ml-5 text-white">
           <Image
@@ -57,6 +58,7 @@ export default function Home() {
           </span>
         </div>
       </nav>
+      {/* LANDING IMAGES */}
       <div className="w-full h-[60vh] -mt-10 flex items-end justify-center">
         <div className="h-[30vh] w-[50vw]  mb-10 flex items-end justify-center ">
           <Image
@@ -79,6 +81,8 @@ export default function Home() {
           />
         </div>
       </div>
+
+      {/* THE FUTURE OF TECHNOLOGY */}
       <div className="w-full h-[40vh] flex flex-col ">
         <div className="w-full flex flex-col absolute bottom-0  ">
           <div className="bg-red w-full h-px"></div>
@@ -93,6 +97,54 @@ export default function Home() {
           </p>
         </div>
       </div>
+      {/* GLOBAL IMPACT... */}
+      <div className="w-full h-[100vh] flex flex-col  justify-center">
+        <p className="h2-style ml-8 mb-[-15px] ">Global Impact of</p>
+        <p className="h2-style -mt-5 ml-8 mb-[-15px]">Cutting-Edge Tech</p>
+      </div>
+      <div className="bg-yellow w-full h-[1px]  flex"> </div>
+      <div className="w-full  flex">
+        <div className="w-[50vw] h-[100vh] ">
+          <p className="h3-style ml-8 mt-24">Artificial Intelligence (AI)</p>
+          <p className="p-style ml-24 mr-24 mt-24">
+            AI is the simulation of human intelligence in machines, programmed
+            to think and learn like humans. It encompasses a wide range of
+            capabilities, including understanding natural language, recognizing
+            patterns, solving problems, and making decisions. AI is used in
+            various applications, such as virtual assistants, self-driving cars,
+            medical diagnostics, and financial analysis. As AI technology
+            continues to advance, it has the potential to revolutionize
+            industries and transform our daily lives.
+          </p>
+          <div className=" flex  mt-24 mr-24 justify-center ">
+            <Image
+              src="/an-ink-drawing-of-an-artificial-intelligence STAR.png"
+              alt="star-L"
+              class="w-auto h-32 mt-[150px]"
+              width={100}
+              height={100}
+            />
+
+            <Image
+              src="/ink-drawing-of-an-artificial-intelligence-assistant.png"
+              alt="middle-image"
+              class="w-auto h-128 object-contain"
+              width={300}
+              height={300}
+            />
+            <Image
+              src="/an-ink-drawing-of-an-artificial-intelligence STAR.png"
+              alt="star-R"
+              class="w-auto h-32 object-contain ml-5"
+              width={100}
+              height={100}
+            />
+          </div>
+        </div>
+        <div className="  bg-red h-[100vh] w-[1px]"></div>
+        <div className="w-[50vw] h-[100vh] "></div>
+      </div>
+      <div className="bg-blue w-full h-[100vh]"></div>
     </div>
   );
 }
