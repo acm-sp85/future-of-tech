eba50579f37a1ea6e2011f73fb0898aa118e71d2
Alex-new-laptop <acontell@gmail.com>
2024-12-04 16:30:13 +0100

    tried again

--- src/app/components/TheFutureOfTechnology.js
+++ src/app/components/TheFutureOfTechnology.js
@@ -8,7 +8,7 @@ function TheFutureOfTechnology() {
         <div className="h-[2px] w-full translate-y-20 bg-yellow md:translate-y-0 lg:mb-[-15px]"></div>
         <span className="h1-style mb-2 ml-3 lg:ml-8">of Technology</span>
       </div>
-      //{" "}
+
       <p className="p-style absolute bottom-[-1px] translate-x-40 sm:translate-x-0 md:right-5">
         &#8595; Scroll Down
       </p>
