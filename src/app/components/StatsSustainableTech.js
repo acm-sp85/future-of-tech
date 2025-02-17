0cb0172aa26b795e681133a97a4d3bf8ba3f250c
Alex-new-laptop <acontell@gmail.com>
2025-02-17 15:23:08 +0100

    no holes in mobile

--- src/app/components/StatsSustainableTech.js
+++ src/app/components/StatsSustainableTech.js
@@ -5,8 +5,8 @@ function StatsSustainableTech() {
     <div className="scrollbar-hide m-6 grid grid-cols-2 gap-4 p-2 lg:h-[100vh] lg:overflow-y-scroll">
       <div className="h-[100%] w-[100%]">
         <div className="relative mx-auto h-[490px] w-[100%]">
-          <div className="shadow-outline mx-auto flex min-h-[300px] lg:min-h-[490px] w-[100%] items-end rounded-3xl ring-2 ring-blue">
-            <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-2 border-blue"></div>
+          <div className="shadow-outline mx-auto flex min-h-[300px] w-[100%] items-end rounded-3xl ring-2 ring-blue lg:min-h-[490px]">
+            <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-blue lg:border-2"></div>
             <p className="h6-style m-10">
               <u>Energy efficiency:</u> Technologies that reduce energy
               consumption, such as LED lighting and smart appliances.
@@ -14,8 +14,8 @@ function StatsSustainableTech() {
           </div>
         </div>
         <div className="relative mx-auto mt-[19px] h-[490px] w-[100%]">
-          <div className="shadow-outline mx-auto flex min-h-[300px] lg:min-h-[490px] w-[100%] items-end rounded-3xl ring-2 ring-red">
-            <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-2 border-red"></div>
+          <div className="shadow-outline mx-auto flex min-h-[300px] w-[100%] items-end rounded-3xl ring-2 ring-red lg:min-h-[490px]">
+            <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-red lg:border-2"></div>
             <p className="h6-style m-10">
               <u>Electric vehicles:</u> Vehicles powered by electricity rather
               than fossil fuels.
@@ -24,8 +24,8 @@ function StatsSustainableTech() {
         </div>
       </div>
       <div className="h-[1000px] w-[100%]">
-        <div className="shadow-outline relative mx-auto flex min-h-[300px] lg:min-h-[1000px] w-[100%] items-end rounded-3xl ring-2 ring-yellow">
-          <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-2 border-yellow"></div>
+        <div className="shadow-outline relative mx-auto flex min-h-[300px] w-[100%] items-end rounded-3xl ring-2 ring-yellow lg:min-h-[1000px]">
+          <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-yellow lg:border-2"></div>
           <p className="h6-style m-10">
             <u>Renewable energy:</u> its sources are derived from natural
             processes that are constantly replenished, making them a sustainable
@@ -35,8 +35,8 @@ function StatsSustainableTech() {
         </div>
       </div>
       <div className="relative col-span-2 h-[300px] w-[100%]">
-        <div className="shadow-outline mx-auto flex min-h-[300px] lg:min-h-[300px] w-[100%] items-end rounded-3xl ring-2 ring-green">
-          <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-2 border-green"></div>
+        <div className="shadow-outline mx-auto flex min-h-[300px] w-[100%] items-end rounded-3xl ring-2 ring-green lg:min-h-[300px]">
+          <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-green lg:border-2"></div>
           <p className="h6-style m-10">
             <u>Sustainable materials:</u> Biodegradable materials, recycled
             materials, and materials with low environmental impact.
@@ -45,8 +45,8 @@ function StatsSustainableTech() {
       </div>
 
       <div className="relative col-span-1 mx-auto h-[600px] w-[100%]">
-        <div className="shadow-outline mx-auto flex min-h-[300px] lg:min-h-[600px] w-[100%] items-end rounded-3xl ring-2 ring-yellow">
-          <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-2 border-yellow"></div>
+        <div className="shadow-outline mx-auto flex min-h-[300px] w-[100%] items-end rounded-3xl ring-2 ring-yellow lg:min-h-[600px]">
+          <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-yellow lg:border-2"></div>
           <p className="h6-style m-10">
             <u>Waste reduction and recycling:</u> Technologies that minimize
             waste generation and maximize recycling efforts.
@@ -54,8 +54,8 @@ function StatsSustainableTech() {
         </div>
       </div>
       <div className="relative col-span-1 mx-auto h-[600px] w-[100%]">
-        <div className="shadow-outline relative mx-auto flex min-h-[300px] lg:min-h-[600px] w-[100%] items-end rounded-3xl ring-2 ring-blue">
-          <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-2 border-blue"></div>
+        <div className="shadow-outline relative mx-auto flex min-h-[300px] w-[100%] items-end rounded-3xl ring-2 ring-blue lg:min-h-[600px]">
+          <div className="absolute right-0 top-0 m-4 h-16 w-16 rounded-full border-blue lg:border-2"></div>
           <p className="h6-style m-10">
             <u>Water conservation:</u> Technologies that reduce water
             consumption and pollution.
